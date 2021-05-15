de124e475bff4783a3caa7488a6666526a6b6449
Charles Gu <guchaojun1@hotmail.com>
2021-05-15 16:38:08 +0800

    check in examples

---
+++ CoreConsole3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CoreConsole3._1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            
+            // Console.WriteLine("\nWhat is your name? "); 
+            // var name = Console.ReadLine(); 
+            // var date = DateTime.Now; 
+            // Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!"); 
+            // Console.Write("\nPress any key to exit..."); 
+            // Console.ReadKey(true);
+
+            var counter = 0; 
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1; 
+            while (max == -1 || counter < max) 
+            { 
+                Console.WriteLine($"Counter: {++counter}"); 
+                await Task.Delay(1000); 
+            }
+        }
+    }
+}
---
+++ CoreWebApplication/CoreWebApplication/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreWebApplication
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+
+            //app.UseOwin(pipeline =>
+            //{
+            //    //pipeline(next => OwinHelloMiddleWare);
+
+            //    pipeline(next =>
+            //    {
+            //        return async environment =>
+            //        {
+            //            // Do something before.
+            //            await OwinHelloMiddleWare(environment);
+
+            //            await next(environment);
+
+            //            // Do something after.
+            //        };
+            //    });
+            //});
+        }
+
+        public Task OwinHelloMiddleWare(IDictionary<string, object> environment)
+        {
+            string responseText = "Hello World via OWIN";
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
+
+            // OWIN Environment Keys: https://owin.org/spec/spec/owin-1.0.0.html
+            var responseStream = (Stream)environment["owin.ResponseBody"];
+            var responseHeaders = (IDictionary<string, string[]>)environment["owin.ResponseHeaders"];
+
+            responseHeaders["Content-Length"] = new string[] { responseBytes.Length.ToString(CultureInfo.InvariantCulture) };
+            responseHeaders["Content-Type"] = new string[] { "text/plain" };
+
+            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
